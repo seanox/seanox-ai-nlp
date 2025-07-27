ced5ef21a50419c39045aa325b11173587c1581c
seanox <service@seanox.com>
2025-07-27 12:41:48 +0200

    #0000 Implementation

---
+++ seanox_ai_nlp/__init__.py
@@ -0,0 +1,3 @@
+# seanox_ai_nlp/__init__.py
+
+from seanox_ai_nlp.units import UNIT_PATTERN, UNIT_WITHOUT_SPACE_PATTERN, units, normalize
---
+++ seanox_ai_nlp/units/__init__.py
@@ -0,0 +1,5 @@
+# seanox_ai_nlp/units/__init__.py
+
+from .units import UNIT_PATTERN, UNIT_WITHOUT_SPACE_PATTERN, units, normalize
+
+__all__ = ["UNIT_PATTERN", "UNIT_WITHOUT_SPACE_PATTERN", "units", "normalize"]

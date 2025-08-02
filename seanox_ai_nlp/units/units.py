eacce91db68b8d8291fa8a2f65e7e9067fe6e706
seanox <service@seanox.com>
2025-08-02 21:39:55 +0200

    #0000 Implementation

--- seanox_ai_nlp/units/units.py
+++ seanox_ai_nlp/units/units.py
@@ -1,259 +1,168 @@
 # seanox_ai_npl/units/units.py
 
 from typing import TypedDict, Union
+from enum import Enum
 
 import re
 
-_UNIT_SYMBOLS_SI_CONFORM_PATTERN = r"(?:A|bar|Bq|C|cd|d|dam|F|g|H|Hz|J|K|l|lm|lx|m|mol|N|\u03A9|Pa|p|rad|s|S|sr|T|t|V|W|Wb)"
-_UNIT_SYMBOLS_SI_NOT_CONFORM_PATTERN = r"(?:a|AE|atm|At\u00FC|bbl|ct|dB|dpt|dz|Dz|ft|gal|ha|h|hl|hp|in|kt|lj|ls|mi|min|oz.|oz.tr.|pc|lb|PS|pt|rm|sq.ft.|sq.in.|sq.mile|sq.yd.|St|u|v|V|yd|Z|\u00B0|\u00B0C|\u2032|\u2033|\u03C1|\u03C9)"
+def _re_compile(expression: str, debug: bool=False) -> re.Pattern:
+    expression = re.sub(r"\s{2,}|[\r\n]+", "", expression)
+    if not debug:
+        return re.compile(expression)
+    return expression
 
-_UNIT_SYMBOLS_SI_CONFORM_FOR_EXPONENTS_PATTERN = r"(?:A|C|dam|F|H|J|K|m|\u03A9|s|T|V)"
-_UNIT_SYMBOLS_SI_NOT_CONFORM_FOR_EXPONENTS_PATTERN = r"(?:a|ft|ha|in|lj|mi|pc|rm|St|v|yd|\u03C9)"
-_UNIT_SYMBOLS_SI_NOT_CONFORM_FOR_INFORMAL_PREFIX_PATTERN = r"(?:a|ft|ha|in|m|mi|pc|rm|yd|St)"
-_UNIT_SYMBOLS_SI_NOT_CONFORM_FOR_INFORMAL_SUFFIX_PATTERN = r"(?:a|ft|ha|in|m|mi|pc|rm|St|v|V|yd|\u03C9)"
-
+_UNIT_SI_SYMBOLS_BASE_PATTERN = r"(?:A|cd|g|K|m|mol|s)"
+_UNIT_SI_SYMBOLS_DERIVATION_PATTERN = r"(?:Bq|C|F|Gy|H|Hz|J|kat|lm|lx|N|\u00B0C|Pa|rad|S|sr|Sv|T|V|W|Wb|\u03A9)"
+_UNIT_SI_SYMBOLS_EXTENSION_PATTERN = r"(?:a|AE|Ah|AU|b|B|bar|ct|d|Da|dB|db\([ACGZ]\)|dpt|eV|h|ha|kn|kt|l|L|mel|min|Np|oz\. tr\.|PS|pt|sone|tex|VA|Wh)"
+_UNIT_SI_SYMBOLS_RELEVANT_PATTERN = r"(?:A|a|AE|Ah|AU|b|B|bar|Bq|C|cd|ct|d|Da|dB|db\([ACGZ]\)|dpt|eV|F|g|Gy|H|h|ha|Hz|J|K|kat|kn|kt|l|L|lm|lx|m|mel|min|mol|N|Np|\u00B0C|oz\. tr\.|Pa|PS|pt|rad|s|S|sone|sr|Sv|T|tex|V|VA|W|Wb|Wh|\u03A9)"
+_UNIT_SI_SYMBOLS_PREFIX_PATTERN = r"(?:A|AE|AU|b|B|Bq|C|cd|ct|Da|eV|F|g|Gy|H|h|Hz|J|K|kat|lm|lx|m|mol|N|\u00B0C|oz\. tr\.|Pa|pt|s|S|sr|Sv|T|V|W|Wb|\u03A9)"
+_UNIT_SI_SYMBOLS_EXPONENTS_PATTERN = r"(?:A|Bq|C|cd|F|Gy|H|Hz|J|K|kat|lm|lx|m|mol|Pa|s|S|sr|Sv|T|V|W|Wb|\u03A9)"
 _UNIT_SI_PREFIX_PATTERN = r"(?:Q|R|Y|Z|E|P|T|G|M|k|h|da|d|c|m|\u00B5|n|p|f|a|z|y|r|q)"
 _UNIT_SI_SUFFIX_PATTERN = r"(?:\u207B?[\u00B9\u00B2\u00B3])"
-_UNIT_INFORMAL_PREFIX_PATTERN = r"(?:q|sq )"
+_UNIT_SI_SYMBOLS_RELEVANT_SET = set(_UNIT_SI_SYMBOLS_RELEVANT_PATTERN[3:-1].split("|"))
+
+_UNIT_COMMON_SYMBOLS_PATTERN = r"(?:\u2032|\u2033|\u2033|a|a|a|Ah|atm|At\u00FC|bar|bbl|d|dam|dB|db\([ACGZ]\)|dpt|dz|Dz|ft|gal|ha|hl|hp|in|kn|kt|kt|l|L|lb|lj|ls|mel|mi|min|Np|\u00B0|oz|oz\.|p|pc|PS|rad|rm|sone|St|t|tex|u|v|VA|Var|Wh|yd|Z|ρ|\u03A9)"
+
+_UNIT_INFORMAL_SYMBOLS_PATTERN = r"(?:a|ft|ha|in|m|mi|mile|yd)"
+_UNIT_INFORMAL_SYMBOLS_SET = set(_UNIT_INFORMAL_SYMBOLS_PATTERN[3:-1].split("|"))
+_UNIT_INFORMAL_SI_SYMBOLS_SET = _UNIT_INFORMAL_SYMBOLS_SET & _UNIT_SI_SYMBOLS_RELEVANT_SET
+_UNIT_INFORMAL_SI_SYMBOLS_PATTERN = rf"(?:{"|".join(_UNIT_INFORMAL_SI_SYMBOLS_SET)})"
+_UNIT_INFORMAL_COMMON_SYMBOLS_SET = _UNIT_INFORMAL_SYMBOLS_SET - _UNIT_SI_SYMBOLS_RELEVANT_SET
+_UNIT_INFORMAL_COMMON_SYMBOLS_PATTERN = rf"(?:{"|".join(_UNIT_INFORMAL_COMMON_SYMBOLS_SET)})"
+_UNIT_INFORMAL_PREFIX_PATTERN = r"(?:c|q|sq\.\s?)"
 _UNIT_INFORMAL_SUFFIX_PATTERN = r"(?:2|3)"
 
-# _UNIT_SYMBOLS_SI_CONFORM_FOR_EXPONENTS_PATTERN may contain _UNIT_SI_PREFIX_PATTERN
-# and _UNIT_SI_SUFFIX_PATTERN
-_UNIT_SI_CONFORM_1 = rf"""
-    {_UNIT_SI_PREFIX_PATTERN}?
-    {_UNIT_SYMBOLS_SI_CONFORM_FOR_EXPONENTS_PATTERN}
-    {_UNIT_SI_SUFFIX_PATTERN}?
-"""
-
-# UNIT_SYMBOLS_SI_CONFORM may contain UNIT_SI_PREFIX_PATTERN
-_UNIT_SI_CONFORM_2 = rf"""
-    {_UNIT_SI_PREFIX_PATTERN}?
-    {_UNIT_SYMBOLS_SI_CONFORM_PATTERN}
-"""
-
-# UNIT_SYMBOLS_SI_CONFORM_PATTERN may occur alone
-_UNIT_SI_CONFORM_3 = rf"""
-    {_UNIT_SYMBOLS_SI_CONFORM_PATTERN}
-"""
-
-_UNIT_SI_CONFORM_PATTERN = rf"""
-    (?:{_UNIT_SI_CONFORM_1}
-    |{_UNIT_SI_CONFORM_2}
-    |{_UNIT_SI_CONFORM_3})
-"""
-
-# _UNIT_SYMBOLS_SI_NOT_CONFORM_FOR_INFORMAL_SUFFIX_PATTERN may contain
-# _UNIT_INFORMAL_PREFIX_PATTERN and _UNIT_INFORMAL_SUFFIX_PATTERN
-_UNIT_SI_NOT_CONFORM_1 = rf"""
-    {_UNIT_INFORMAL_PREFIX_PATTERN}?
-    {_UNIT_SYMBOLS_SI_NOT_CONFORM_FOR_INFORMAL_SUFFIX_PATTERN}
-    {_UNIT_INFORMAL_SUFFIX_PATTERN}?
-"""
-
-# _UNIT_SYMBOLS_SI_NOT_CONFORM_FOR_INFORMAL_PREFIX_PATTERN may contain
-# _UNIT_INFORMAL_PREFIX_PATTERN
-_UNIT_SI_NOT_CONFORM_2 = rf"""
-    {_UNIT_INFORMAL_PREFIX_PATTERN}?
-    {_UNIT_SYMBOLS_SI_NOT_CONFORM_FOR_INFORMAL_PREFIX_PATTERN}
-"""
-
-# _UNIT_SYMBOLS_SI_NOT_CONFORM_PATTERN must not use _UNIT_SI_PREFIX_PATTERN
-
-# _UNIT_SYMBOLS_SI_NOT_CONFORM_FOR_EXPONENTS_PATTERN may contain
-# _UNIT_SI_SUFFIX_PATTERN
-_UNIT_SI_NOT_CONFORM_3 = rf"""
-    {_UNIT_SYMBOLS_SI_NOT_CONFORM_FOR_EXPONENTS_PATTERN}?
-    {_UNIT_SI_SUFFIX_PATTERN}
-"""
-
-# _UNIT_SYMBOLS_SI_NOT_CONFORM_PATTERN may occur alone
-_UNIT_SI_NOT_CONFORM_4 = rf"""
-    {_UNIT_SYMBOLS_SI_NOT_CONFORM_PATTERN}
-"""
-
-_UNIT_SI_NOT_CONFORM_PATTERN = rf"""
-    (?:{_UNIT_SI_NOT_CONFORM_1}
-        |{_UNIT_SI_NOT_CONFORM_2}
-        |{_UNIT_SI_NOT_CONFORM_3}
-        |{_UNIT_SI_NOT_CONFORM_4})
-"""
-
-_UNIT_VARIANT_PATTERN = rf"""
-    (?:{_UNIT_SI_CONFORM_PATTERN}
-        |{_UNIT_SI_NOT_CONFORM_PATTERN})
-"""
-
-
-def _re_compile(expression: str) -> re.Pattern:
-    return re.compile(
-        re.sub(
-            r"\s{2,}|[\r\n]+",
-            "",
-            expression)
+_UNIT_INFORMAL_SI_RAW_PATTERN = rf"""
+    (?:
+      {_UNIT_INFORMAL_PREFIX_PATTERN}?
+      {_UNIT_SI_PREFIX_PATTERN}?
+      {_UNIT_INFORMAL_SI_SYMBOLS_PATTERN}
+      (?:
+        {_UNIT_INFORMAL_SUFFIX_PATTERN}
+        |{_UNIT_SI_SUFFIX_PATTERN} 
+      )?
     )
+"""
 
+_UNIT_INFORMAL_COMMON_RAW_PATTERN = rf"""
+    (?:
+      {_UNIT_INFORMAL_PREFIX_PATTERN}?
+      {_UNIT_INFORMAL_COMMON_SYMBOLS_PATTERN}
+      {_UNIT_INFORMAL_SUFFIX_PATTERN}?
+    )
+"""
 
-_UNIT_MATHEMATICAL_OPERATORS_PATTERN = r"[\u00B7\u002F]"
+_UNIT_INFORMAL_RAW_PATTERN = rf"""
+    (?:
+      {_UNIT_INFORMAL_SI_RAW_PATTERN}
+      |{_UNIT_INFORMAL_COMMON_RAW_PATTERN}
+    )
+"""
+
+_UNIT_MATHEMATICAL_OPERATORS_PATTERN = r"[\u00B7\u00D7\u002A\u0078\u002F]"
+
 _UNIT_LOOK_AHEAD_PATTERN = r"(?:(?<=[\W\d])|^)"
 _UNIT_LOOK_AHEAD_WITHOUT_SPACE_PATTERN = r"(?<=\d)"
 _UNIT_LOOK_BEHIND_PATTERN = r"(?:(?=[^\w\u00B7\u002F])|$)"
 _UNIT_MULTIPLE_SPACES = r"(?:\s{2,})"
 
-UNIT_PATTERN = _re_compile(rf"""
-    {_UNIT_LOOK_AHEAD_PATTERN}
-    ({_UNIT_VARIANT_PATTERN}
-        (?:{_UNIT_MATHEMATICAL_OPERATORS_PATTERN}
-            {_UNIT_VARIANT_PATTERN})*)
-    {_UNIT_LOOK_BEHIND_PATTERN}
-""")
-
-_UNIT_WITHOUT_SPACE_PATTERN = _re_compile(rf"""
-    {_UNIT_LOOK_AHEAD_WITHOUT_SPACE_PATTERN}
-    {_UNIT_VARIANT_PATTERN}
-    (?:{_UNIT_MATHEMATICAL_OPERATORS_PATTERN}
-        {_UNIT_VARIANT_PATTERN})*
-    {_UNIT_LOOK_BEHIND_PATTERN}
-""")
-
-_UNIT_WITH_MULTIPLE_SPACES_PATTERN = _re_compile(rf"""
-    {_UNIT_LOOK_AHEAD_PATTERN}
-    ({_UNIT_MULTIPLE_SPACES})
-    ({_UNIT_VARIANT_PATTERN}
-        (?:{_UNIT_MATHEMATICAL_OPERATORS_PATTERN}
-            {_UNIT_VARIANT_PATTERN})*)
-    {_UNIT_LOOK_BEHIND_PATTERN}
-""")
-
-_UNIT_INFORMAL_PREFIX_PART_PATTERN = _re_compile(rf"""
-    (?:^|{_UNIT_MATHEMATICAL_OPERATORS_PATTERN})
-    {_UNIT_INFORMAL_PREFIX_PATTERN}
-""")
-
-_UNIT_INFORMAL_SUFFIX_PART_PATTERN = _re_compile(rf"""
-    {_UNIT_INFORMAL_SUFFIX_PATTERN}
-    (?:{_UNIT_MATHEMATICAL_OPERATORS_PATTERN}|$)
-""")
-
-_NUMERIC_LOOK_AHEAD_PATTERN = r"(?:(?<![\u00B1+\-,.\u2019\w\d])|^)"
+_NUMERIC_LOOK_AHEAD_PATTERN = r"(?:(?<![\u00B1+\-,\.\u2019\w\d])|^)"
 _NUMERIC_SIGN_PATTERN = r"[\u00B1+\-]"
 _NUMERIC_DE_PATTERN = r"(?:(?:(?:\d{1,3}(?:\.\d{3})*)|\d+)(?:,\d+)?)"
 _NUMERIC_EN_PATTERN = r"(?:(?:(?:\d{1,3}(?:,\d{3})*)|\d+)(?:\.\d+))?"
 _NUMERIC_CH_PATTERN = r"(?:(?:(?:\d{1,3}(?:\u2019\d{3})*)|\d+)(?:,\d+)?)"
 
-NUMERIC_UNIT_PATTERN = _re_compile(rf"""
-    {_NUMERIC_LOOK_AHEAD_PATTERN} 
-    ({_NUMERIC_SIGN_PATTERN})?
-    ({_NUMERIC_DE_PATTERN}|{_NUMERIC_EN_PATTERN}|{_NUMERIC_CH_PATTERN})
-    (?:\s*)
-    ({_UNIT_VARIANT_PATTERN}
-        (?:{_UNIT_MATHEMATICAL_OPERATORS_PATTERN}
-            {_UNIT_VARIANT_PATTERN})*)
+_UNIT_SI_RAW_PATTERN = rf"""
+    (?:{_UNIT_SI_PREFIX_PATTERN}?
+    {_UNIT_SI_SYMBOLS_RELEVANT_PATTERN}
+    {_UNIT_SI_SUFFIX_PATTERN}?)
+"""
+
+_UNIT_COMMON_RAW_PATTERN = rf"""
+    (?:{_UNIT_COMMON_SYMBOLS_PATTERN})
+"""
+
+_UNIT_RAW_PATTERN = rf"""
+    (?:
+      {_UNIT_SI_RAW_PATTERN}
+      |{_UNIT_INFORMAL_RAW_PATTERN}
+      |{_UNIT_COMMON_RAW_PATTERN}
+    )
+"""
+
+_UNIT_EXPRESSION_RAW_PATTERN = rf"""
+    (?:
+      {_UNIT_RAW_PATTERN}
+      (?:
+        {_UNIT_MATHEMATICAL_OPERATORS_PATTERN}
+        {_UNIT_RAW_PATTERN}
+      )*
+    )
+"""
+
+_NUMERIC_VARIANTS_RAW_PATTERN = rf"""
+    (?:
+      {_NUMERIC_SIGN_PATTERN}?
+      (?:
+        {_NUMERIC_DE_PATTERN}
+        |{_NUMERIC_EN_PATTERN}
+        |{_NUMERIC_CH_PATTERN}
+      )    
+    )
+"""
+
+_UNIT_VALUE_PART_PATTERN = rf"""
+    {_NUMERIC_LOOK_AHEAD_PATTERN}
+    (?P<unitValueNumericValue>{_NUMERIC_VARIANTS_RAW_PATTERN})
+    (?P<unitValueUnit>{_UNIT_EXPRESSION_RAW_PATTERN})
+    {_UNIT_LOOK_BEHIND_PATTERN}
+"""
+
+_UNIT_PART_PATTERN = rf"""
+    {_UNIT_LOOK_AHEAD_PATTERN}
+    (?P<unitUnit>{_UNIT_EXPRESSION_RAW_PATTERN})
+    {_UNIT_LOOK_BEHIND_PATTERN}
+"""
+
+UNIT_PATTERN = _re_compile(rf"""
+    {_UNIT_VALUE_PART_PATTERN}
+    |{_UNIT_PART_PATTERN}
+""")
+
+_UNIT_WITH_INVALID_SPACES_NUMERIC_PATTERN = _re_compile(rf"""
+    (?<=\d)(\s{{2,}})?
+    {_UNIT_EXPRESSION_RAW_PATTERN}
     {_UNIT_LOOK_BEHIND_PATTERN}
 """)
 
-_NUMERIC_LEADING_ZEROS_ANTI_PATTERN = _re_compile(rf"^0\d")
-_NUMERIC_INVALID_DECIMAL_THOUSAND_SEPARATOR_ANTI_PATTERN = _re_compile(rf"""
-   (\d+\.\d+,\d+\.)
-   |(\d+,\d+\.\d+,)
-   |(\d+\u2019\d+,?\d+\u2019)
+_UNIT_WITH_INVALID_SPACES_ALPHANUMERIC_PATTERN = _re_compile(rf"""
+    (?:
+      (?:
+        (?<=\d)(\s{{2,}})?)
+        |(?:(?<=\w)(\s{{2,}})
+      )
+    )
+    {_UNIT_EXPRESSION_RAW_PATTERN}
+    {_UNIT_LOOK_BEHIND_PATTERN}
 """)
 
-
-class UnitValue(TypedDict):
-    label: str
-    start: int
-    end: int
-    text: str
-    value: str
-    unit: str
-    normalized: str
-    conform: bool
-    informal: bool
-
-
-class Unit(TypedDict):
-    label: str
-    start: int
-    end: int
-    text: str
-    unit: str
-    normalized: str
-    conform: bool
-    informal: bool
-
-
-UnitEntry = Union[UnitValue, Unit]
-
-
-def units(text: str) -> list[UnitEntry]:
-
-    if not text:
-        return []
-
-    entities = []
-    for match in NUMERIC_UNIT_PATTERN.finditer(text):
-        sign, value, unit = match.groups()
-        # Values with leading zeros before other numbers are invalid.
-        if _NUMERIC_LEADING_ZEROS_ANTI_PATTERN.match(value):
-            continue
-        # Invalid decimal point/thousand separator combinations are not allowed
-        if _NUMERIC_INVALID_DECIMAL_THOUSAND_SEPARATOR_ANTI_PATTERN.search(value):
-            continue
-        # Invalid thousand blocks are already ignored by the base expression
-
-        # Units that appear more than once are not allowed
-        subunits = re.split(_UNIT_MATHEMATICAL_OPERATORS_PATTERN, unit)
-        if any(subunits.count(s) > 1 for s in set(subunits)):
-            continue
-
-        conform = False
-        for sub in subunits:
-            if not re.fullmatch(_UNIT_SYMBOLS_SI_CONFORM_PATTERN, sub):
-                conform = False
-                break
-            else:
-                conform = True
-
-        informal = bool(
-            _UNIT_INFORMAL_PREFIX_PART_PATTERN.search(unit)
-            or _UNIT_INFORMAL_SUFFIX_PART_PATTERN.search(unit)
-        )
-
-        entity: UnitValue = {
-            "label": "UNIT-VALUE",
-            "start": match.start(),
-            "end": match.end(),
-            "text": match.group(),
-            "value": f"{sign or ''}{value}",
-            "unit": unit,
-            "normalized": normalize(match.group().strip()),
-            "conform": conform,
-            "informal": informal
-        }
-        entities.append(entity)
-
-        entity: Unit = {
-            "label": "UNIT",
-            "start": match.start(3),
-            "end": match.end(3),
-            "text": match.group(3),
-            "unit": unit,
-            "normalized": normalize(match.group(3).strip()),
-            "conform": conform,
-            "informal": informal
-        }
-        entities.append(entity)
-
-    return entities
-
-
-def normalize(text: str) -> str:
-    text = _UNIT_WITHOUT_SPACE_PATTERN.sub(
-        lambda match: " " + match.group(0),
-        text
+_UNIT_WITH_INVALID_SPACES_ALL_PATTERN = _re_compile(rf"""
+    (?:
+      (?:(?<=\d)(\s{{2,}})?{_UNIT_EXPRESSION_RAW_PATTERN})
+      |(?:\s{{2,}}{_UNIT_EXPRESSION_RAW_PATTERN})
+      |(?:(?<![\w/*]){_UNIT_EXPRESSION_RAW_PATTERN})
     )
-    text = _UNIT_WITH_MULTIPLE_SPACES_PATTERN.sub(
-        lambda match: " " + match.group(2),
+    {_UNIT_LOOK_BEHIND_PATTERN}  
+""")
+
+class SpacingMode(Enum):
+    NUMERIC = _UNIT_WITH_INVALID_SPACES_NUMERIC_PATTERN
+    ALPHANUMERIC = _UNIT_WITH_INVALID_SPACES_ALPHANUMERIC_PATTERN
+    ALL = _UNIT_WITH_INVALID_SPACES_ALL_PATTERN
+
+def spacing(text: str, mode: SpacingMode=SpacingMode.NUMERIC) -> str:
+    text = mode.value.sub(
+        lambda match: " " + match.group(0).strip(),
         text
     )
     return text
--- tests/test_units.py
+++ tests/test_units.py
@@ -1,9 +1,21 @@
 # tests/test_units.py
 
-from seanox_ai_nlp.units import normalize, units
+from seanox_ai_nlp.units import SpacingMode, spacing, units
 
 import pytest
 import re
+
+def _parse_test_cases(data):
+    cases = []
+    for line in data.strip().splitlines():
+        line = line.strip()
+        if not line or line.startswith("#"):
+            continue
+        match = re.split(r"\s*->\s*", line, maxsplit=1)
+        if len(match) == 2:
+            input, expected = match
+            cases.append((input.strip(), expected.strip()))
+    return cases
 
 _TEST_CASES_01 = """
 # simple units
@@ -82,13 +94,13 @@ xxx  km/s            -> xxx  km/s
 # simple units with commas in values
 xxx 100,m            -> xxx 100,m
 xxx 100, m           -> xxx 100, m
-xxx 100,  m          -> xxx 100, m
+xxx 100,  m          -> xxx 100,  m
 xxx 100,km           -> xxx 100,km
 xxx 100, km          -> xxx 100, km
-xxx 100,  km         -> xxx 100, km
+xxx 100,  km         -> xxx 100,  km
 xxx 100,km/s         -> xxx 100,km/s
 xxx 100, km/s        -> xxx 100, km/s
-xxx 100,  km/s       -> xxx 100, km/s
+xxx 100,  km/s       -> xxx 100,  km/s
 
 # simple units with commas as decimals
 xxx 100,0m           -> xxx 100,0 m
@@ -104,13 +116,13 @@ xxx 100,0  km/s/m2   -> xxx 100,0 km/s/m2
 # simple units with decimal point but without decimal place
 xxx 100.m            -> xxx 100.m
 xxx 100. m           -> xxx 100. m
-xxx 100.  m          -> xxx 100. m
+xxx 100.  m          -> xxx 100.  m
 xxx 100.km           -> xxx 100.km
 xxx 100. km          -> xxx 100. km
-xxx 100.  km         -> xxx 100. km
+xxx 100.  km         -> xxx 100.  km
 xxx 100.km/s         -> xxx 100.km/s
 xxx 100. km/s        -> xxx 100. km/s
-xxx 100.  km/s       -> xxx 100. km/s
+xxx 100.  km/s       -> xxx 100.  km/s
 
 # simple units with decimal point
 xxx 100.0m           -> xxx 100.0 m
@@ -122,22 +134,143 @@ xxx 100.0  km        -> xxx 100.0 km
 xxx 100.0km/s        -> xxx 100.0 km/s
 xxx 100.0 km/s       -> xxx 100.0 km/s
 xxx 100.0  km/s      -> xxx 100.0 km/s
+
+xxx 100.0  Ah/s      -> xxx 100.0 Ah/s
+xxx 100.0  hW/s      -> xxx 100.0 hW/s
 """
 
 
-def _parse_test_cases(data):
-    cases = []
-    for line in data.strip().splitlines():
-        line = line.strip()
-        if not line or line.startswith("#"):
-            continue
-        match = re.split(r"\s*->\s*", line, maxsplit=1)
-        if len(match) == 2:
-            input, expected = match
-            cases.append((input.strip(), expected.strip()))
-    return cases
-
 
 @pytest.mark.parametrize("input,expected", _parse_test_cases(_TEST_CASES_01))
-def test_normalize(input, expected):
-    assert normalize(input) == expected
+def test_normalize_01_a(input, expected):
+    assert spacing(input) == expected
+
+@pytest.mark.parametrize("input,expected", _parse_test_cases(_TEST_CASES_01))
+def test_normalize_01_b(input, expected):
+    assert spacing(input, SpacingMode.NUMERIC) == expected
+
+_TEST_CASES_02 = """
+# simple units
+100m                 -> 100 m
+100 m                -> 100 m
+100  m               -> 100 m
+100km                -> 100 km
+100 km               -> 100 km
+100  km              -> 100 km
+100km/s              -> 100 km/s
+100 km/s             -> 100 km/s
+100  km/s            -> 100 km/s
+
+# simple units with invalid prefix
+x100m                -> x100 m
+x100 m               -> x100 m
+x100  m              -> x100 m
+x100km               -> x100 km
+x100 km              -> x100 km
+x100  km             -> x100 km
+x100km/s             -> x100 km/s
+x100 km/s            -> x100 km/s
+x100  km/s           -> x100 km/s
+
+# simple units with invalid suffix
+x100mx               -> x100mx
+x100 mx              -> x100 mx
+x100  mx             -> x100  mx
+x100kmx              -> x100kmx
+x100 kmx             -> x100 kmx
+x100  kmx            -> x100  kmx
+x100km/sx            -> x100km/sx
+x100 km/sx           -> x100 km/sx
+x100  km/sx          -> x100  km/sx
+
+# simple units with downstream text
+100m xxx             -> 100 m xxx
+100 m xxx            -> 100 m xxx
+100  m xxx           -> 100 m xxx
+100km xxx            -> 100 km xxx
+100 km xxx           -> 100 km xxx
+100  km xxx          -> 100 km xxx
+100km/s xxx          -> 100 km/s xxx
+100 km/s xxx         -> 100 km/s xxx
+100  km/s xxx        -> 100 km/s xxx
+
+# simple units embedded
+xxx 100m xxx         -> xxx 100 m xxx
+xxx 100 m xxx        -> xxx 100 m xxx
+xxx 100  m xxx       -> xxx 100 m xxx
+xxx 100km xxx        -> xxx 100 km xxx
+xxx 100 km xxx       -> xxx 100 km xxx
+xxx 100  km xxx      -> xxx 100 km xxx
+xxx 100km/s xxx      -> xxx 100 km/s xxx
+xxx 100 km/s xxx     -> xxx 100 km/s xxx
+xxx 100  km/s xxx    -> xxx 100 km/s xxx
+xxx 100 km\u00B7s/ft -> xxx 100 km\u00B7s/ft
+xxx 100  km\u00B7s   -> xxx 100 km\u00B7s
+
+# simple units with preceding text
+xxx 100m             -> xxx 100 m
+xxx 100 m            -> xxx 100 m
+xxx 100  m           -> xxx 100 m
+xxx 100km            -> xxx 100 km
+xxx 100 km           -> xxx 100 km
+xxx 100  km          -> xxx 100 km
+xxx 100km/s          -> xxx 100 km/s
+xxx 100 km/s/ft      -> xxx 100 km/s/ft
+xxx 100  km/s        -> xxx 100 km/s
+
+# simple units without values (invalid)
+xxxkm/s              -> xxxkm/s
+xxx km/s             -> xxx km/s
+xxx  km/s            -> xxx km/s
+
+# simple units with commas in values
+xxx 100,m            -> xxx 100,m
+xxx 100, m           -> xxx 100, m
+xxx 100,  m          -> xxx 100,  m
+xxx 100,km           -> xxx 100,km
+xxx 100, km          -> xxx 100, km
+xxx 100,  km         -> xxx 100,  km
+xxx 100,km/s         -> xxx 100,km/s
+xxx 100, km/s        -> xxx 100, km/s
+xxx 100,  km/s       -> xxx 100,  km/s
+
+# simple units with commas as decimals
+xxx 100,0m           -> xxx 100,0 m
+xxx 100,0 m          -> xxx 100,0 m
+xxx 100,0  m         -> xxx 100,0 m
+xxx 100,0km          -> xxx 100,0 km
+xxx 100,0 km         -> xxx 100,0 km
+xxx 100,0  km        -> xxx 100,0 km
+xxx 100,0km/s/ft     -> xxx 100,0 km/s/ft
+xxx 100,0 km/s/qm    -> xxx 100,0 km/s/qm
+xxx 100,0  km/s/m2   -> xxx 100,0 km/s/m2
+
+# simple units with decimal point but without decimal place
+xxx 100.m            -> xxx 100.m
+xxx 100. m           -> xxx 100. m
+xxx 100.  m          -> xxx 100.  m
+xxx 100.km           -> xxx 100.km
+xxx 100. km          -> xxx 100. km
+xxx 100.  km         -> xxx 100.  km
+xxx 100.km/s         -> xxx 100.km/s
+xxx 100. km/s        -> xxx 100. km/s
+xxx 100.  km/s       -> xxx 100.  km/s
+
+# simple units with decimal point
+xxx 100.0m           -> xxx 100.0 m
+xxx 100.0 m          -> xxx 100.0 m
+xxx 100.0  m         -> xxx 100.0 m
+xxx 100.0km          -> xxx 100.0 km
+xxx 100.0 km         -> xxx 100.0 km
+xxx 100.0  km        -> xxx 100.0 km
+xxx 100.0km/s        -> xxx 100.0 km/s
+xxx 100.0 km/s       -> xxx 100.0 km/s
+xxx 100.0  km/s      -> xxx 100.0 km/s
+
+xxx 100.0  Ah/s      -> xxx 100.0 Ah/s
+xxx 100.0  hW/s      -> xxx 100.0 hW/s
+"""
+
+@pytest.mark.parametrize("input,expected", _parse_test_cases(_TEST_CASES_02))
+def test_normalize_02(input, expected):
+    assert spacing(input, SpacingMode.ALPHANUMERIC) == expected

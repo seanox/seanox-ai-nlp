7c98fd86a5973a73a704a268f65e2455981116b1
seanox <service@seanox.com>
2025-10-26 08:49:20 +0100

    #0000 relations: Implementation

--- seanox_ai_nlp/relations/lang/abstract.py
+++ seanox_ai_nlp/relations/lang/abstract.py
@@ -1,11 +1,15 @@
 # seanox_ai_npl/relations/abstract.py
 
 from abc import ABC, abstractmethod
+from typing import Callable
 
 from stanza.models.common.doc import Sentence, Word
 
 
 class Relations(ABC):
+
+    def sentence_preprocessor(self) -> Callable[[Sentence], list[str]] | None:
+        return None
 
     @abstractmethod
     def find_markers(self, sentence: Sentence, word: Word) -> frozenset[str]:

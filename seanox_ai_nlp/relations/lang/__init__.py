35f04c60d492afa1f288cd9a73184a607c024981
seanox <service@seanox.com>
2025-10-30 08:36:10 +0100

    #0000 relations: Implementation

--- seanox_ai_nlp/relations/lang/__init__.py
+++ seanox_ai_nlp/relations/lang/__init__.py
@@ -1,38 +1,38 @@
 # seanox_ai_nlp/relations/lang/__init__.py
 
-from .abstract import Relations
-from .de import RelationsDE
-from .dk import RelationsDK
-from .en import RelationsEN
-from .es import RelationsES
-from .fr import RelationsFR
-from .it import RelationsIT
-from .ru import RelationsRU
+from .abstract import Schema
+from .de import SchemaDE
+from .dk import SchemaDK
+from .en import SchemaEN
+from .es import SchemaES
+from .fr import SchemaFR
+from .it import SchemaIT
+from .ru import SchemaRU
 
 
-_RELATIONS: dict[str, type[Relations]] = {
-    "de": RelationsDE,
-    "dk": RelationsDK,
-    "en": RelationsEN,
-    "es": RelationsES,
-    "fr": RelationsFR,
-    "it": RelationsIT,
-    "ru": RelationsRU
+_SCHEMAS: dict[str, type[Schema]] = {
+    "de": SchemaDE,
+    "dk": SchemaDK,
+    "en": SchemaEN,
+    "es": SchemaES,
+    "fr": SchemaFR,
+    "it": SchemaIT,
+    "ru": SchemaRU
 }
 
 
 def languages() -> frozenset[str]:
-    return frozenset(_RELATIONS.keys())
+    return frozenset(_SCHEMAS.keys())
 
 
-def language_module(lang: str) -> Relations:
-    if lang not in _RELATIONS.keys():
+def language_schema(lang: str) -> Schema:
+    if lang not in _SCHEMAS:
         raise ValueError(f"Unsupported language: {lang}")
-    return _RELATIONS[lang]()
+    return _SCHEMAS[lang]()
 
 
 __all__ = [
-    "Relations",
+    "Schema",
     "languages",
-    "language_module"
+    "language_schema"
 ]
